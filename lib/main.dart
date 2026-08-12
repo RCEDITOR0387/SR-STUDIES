314ab96532c9ac4115e68644501239b0f7272999
RCEDITOR0387 <r3602096@gmail.com>
2026-08-12 11:48:25 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,54 @@
 
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const SRStudiesApp());
+}
+
+class SRStudiesApp extends StatelessWidget {
+  const SRStudiesApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'SR STUDIES',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
+        useMaterial3: true,
+      ),
+      home: const HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('SR STUDIES'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: const [
+            Icon(Icons.school, size: 80),
+            SizedBox(height: 20),
+            Text(
+              'SR STUDIES',
+              style: TextStyle(
+                fontSize: 28,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 10),
+            Text('Official Coaching & Study App'),
+          ],
+        ),
+      ),
+    );
+  }
+}
